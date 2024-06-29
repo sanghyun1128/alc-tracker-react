f89277bbfa62b3b3a4ae4f08932d2ff0c003f5f2
sanghyun1128 <sanghyun1128@gmail.com>
2024-06-29 21:14:21 +0900

    feat: add dark and light themes

---
+++ src/themes/darkTheme.ts
@@ -0,0 +1,26 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  alt: 'dark',
+  borderRadius: '16px',
+  padding: '2vh',
+  border: '3px solid #4b4b4b8a',
+  borderResultList: '0px solid black',
+  themeToggle: '2px solid #FFFFFF',
+  iconInvert: '100%',
+
+  colors: {
+    default: '#FFFFFF',
+    primary: '#FF9F29',
+    prhover: '#E07900',
+    container: '#363739',
+    text: '#F4F7FA',
+    button: '#43454e',
+    btnhover: '#6C768C',
+    header: '#202123',
+    opposite: '#808080',
+    background: '#2c2d2c',
+    inputBackground: '#f0f0f0',
+    placeHolder: '#F4F7FA',
+  },
+};
---
+++ src/themes/lightTheme.ts
@@ -0,0 +1,26 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  alt: 'light',
+  borderRadius: '16px',
+  padding: '2vh',
+  border: '3px solid #F4F7FA',
+  borderResultList: '0px solid black',
+  themeToggle: '2px solid #F4F7FA',
+  iconInvert: '0%',
+
+  colors: {
+    default: '#000000',
+    primary: '#E07900',
+    prhover: '#FF8A00',
+    container: '#FFFFFF',
+    text: '#43454e',
+    button: '#F4F7FA',
+    btnhover: '#D5D9E0',
+    header: '#F8F8F8',
+    opposite: '#FFFFFF',
+    background: '#F4F7FA',
+    inputBackground: '#FFFFFF',
+    placeHolder: '#808080',
+  },
+};
