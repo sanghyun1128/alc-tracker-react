07f9d9388c18ad300ca4574d2223e09b46b035f9
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-01 21:17:41 +0900

    feat: add email and password validation logic before submit

--- src/forms/LoginForm.tsx
+++ src/forms/LoginForm.tsx
@@ -7,6 +7,8 @@ import InformationInput from '../components/InformationInput';
 import InformationLabel from '../components/InformationLabel';
 import SubmitButton from '../components/SubmitButton';
 import { deviceSizes } from '../const/deviceSizes';
+import { emailValidation } from '../validation';
+import { passwordValidation } from '../validation/passwordValidation';
 
 const Form = styled.form`
   display: grid;
@@ -47,27 +49,61 @@ interface LoginFormProps {
   theme: DefaultTheme;
 }
 
+//TODO: Password 암호화해서 서버로 전송하기
 export default function LoginForm({ theme }: LoginFormProps) {
-  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [isEmailError, setIsEmailError] = useState<boolean>(false);
+  const [isPasswordError, setIsPasswordError] = useState<boolean>(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(prevShowPassword => !prevShowPassword);
   };
 
+  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (emailValidation(email)) {
+      console.log('Email is valid');
+      setIsEmailError(false);
+    } else {
+      console.log('Email is invalid');
+      setIsEmailError(true);
+    }
+
+    if (passwordValidation(password)) {
+      console.log('Password is valid');
+      setIsPasswordError(false);
+    } else {
+      console.log('Password is invalid');
+      setIsPasswordError(true);
+    }
+  };
+
   return (
-    <Form theme={theme}>
-      <InformationLabel text="Email" gridcolumn="1 / 6" gridrow="1 / 2" />
-      <InformationInput type="email" gridcolumn="1 / 6" gridrow="2 / 3" />
-      <InformationLabel text="Password" gridcolumn="1 / 6" gridrow="3 / 4" />
+    <Form theme={theme} onSubmit={event => submitForm(event)}>
+      <InformationLabel text="Email" gridColumn="1 / 6" gridRow="1 / 2" />
+      <InformationInput
+        type="email"
+        maxLength={254}
+        gridColumn="1 / 6"
+        gridRow="2 / 3"
+        isError={isEmailError}
+        onChange={event => setEmail(event.target.value)}
+      />
+      <InformationLabel text="Password" gridColumn="1 / 6" gridRow="3 / 4" />
       <InformationInput
         type={showPassword ? 'text' : 'password'}
-        gridcolumn="1 / 6"
-        gridrow="4 / 5"
+        maxLength={20}
+        gridColumn="1 / 6"
+        gridRow="4 / 5"
+        isError={isPasswordError}
+        onChange={event => setPassword(event.target.value)}
       />
       <ToggleButton type="button" onClick={togglePasswordVisibility}>
         {showPassword ? 'Hide' : 'Show'}
       </ToggleButton>
-      <SubmitButton text="Log In" gridcolumn="2 / 5" gridrow="5 / 6" />
+      <SubmitButton text="Log In" gridColumn="2 / 5" gridRow="5 / 6" />
     </Form>
   );
 }
