89a80cae0a1a1a09de506de754820f79f2dd3684
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-01 18:01:56 +0900

    design: update font-family in GlobalStyle

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -6,6 +6,14 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     width: 100vw;
     height: 100vh;
+    font-family:
+    system-ui,
+    -apple-system,
+    system-ui,
+    'Helvetica Neue',
+    Helvetica,
+    Arial,
+    sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
