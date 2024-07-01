f3f8a47316e0d3818fdc7d81f383a2de882eca71
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-01 14:46:31 +0900

    style: apply code formatting

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
