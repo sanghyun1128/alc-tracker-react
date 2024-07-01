f06b715b08c677c646c5b9415e4d266a56656f7b
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-01 21:14:05 +0900

    feat: add shake animation

--- src/animations/basicAnimations.ts
+++ src/animations/basicAnimations.ts
@@ -9,5 +9,22 @@ export const fadeInUp = keyframes`
       opacity: 1;
       transform: translateZ(0);
   }
+`;
 
+export const shake = keyframes`
+  0% {
+      transform: translateX(0);
+  }
+  25% {
+      transform: translateX(-5px);
+  }
+  50% {
+      transform: translateX(5px);
+  }
+  75% {
+      transform: translateX(-5px);
+  }
+  to {
+      transform: translateX(0);
+  }
 `;
