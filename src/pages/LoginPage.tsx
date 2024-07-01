eec15c36f6b7c430c35b8528dac0d49efe6559ab
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-01 18:07:50 +0900

    feat: set theme and load the login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
+import { styled } from 'styled-components';
+
+import LoginForm from '../forms/LoginForm';
+import { useTheme } from '../hooks/useTheme';
+
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${props => props.theme.colors.background};
+`;
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [theme] = useTheme();
+
+  return (
+    <Container theme={theme}>
+      <LoginForm theme={theme} />
+    </Container>
+  );
 }
