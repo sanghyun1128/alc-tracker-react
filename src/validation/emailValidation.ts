0ab8adf4f9bdd767240ac4fd8bbdc0739d857d06
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-02 13:05:24 +0900

    docs: add comments about validation function

--- src/validation/emailValidation.ts
+++ src/validation/emailValidation.ts
@@ -1,4 +1,10 @@
-export const emailValidation = (email: string) => {
+/**
+ * Check input email is following email format
+ *
+ * @param {string} email
+ * @returns {boolean}
+ */
+export const emailValidation = (email: string): boolean => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(String(email).toLowerCase());
 };
