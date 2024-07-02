bc4c14528115a1a97aad18edf6c2208a6c5dbce3
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-02 14:11:44 +0900

    refactor: update because of included components changed

--- src/forms/LoginForm.tsx
+++ src/forms/LoginForm.tsx
@@ -6,6 +6,7 @@ import { fadeInUp } from '../animations/basicAnimations';
 import InformationInput from '../components/InformationInput';
 import InformationLabel from '../components/InformationLabel';
 import SubmitButton from '../components/SubmitButton';
+import TextButton from '../components/TextButton';
 import { deviceSizes } from '../const/deviceSizes';
 import { emailValidation } from '../validation';
 import { passwordValidation } from '../validation/passwordValidation';
@@ -13,7 +14,7 @@ import { passwordValidation } from '../validation/passwordValidation';
 const Form = styled.form`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: 0.5fr 0.5fr 0.5fr 0.5fr 1fr;
+  grid-template-rows: repeat(6, 1fr);
   align-items: center;
   justify-items: stretch;
   height: 400px;
@@ -24,25 +25,16 @@ const Form = styled.form`
 
   ${deviceSizes.small} {
     width: 250px;
-    padding: 20px;
+    padding: 20px 20px 0px 20px;
   }
   ${deviceSizes.medium} {
     width: 400px;
-    padding: 50px;
+    padding: 50px 50px 0px 50px;
   }
   ${deviceSizes.large} {
     width: 500px;
-    padding: 50px;
+    padding: 50px 50px 0px 50px;
   }
-`;
-
-const ToggleButton = styled.button`
-  grid-column: 5 / 6;
-  grid-row: 4 / 5;
-  background: none;
-  border: none;
-  cursor: pointer;
-  color: ${props => props.theme.colors.primary};
 `;
 
 interface LoginFormProps {
@@ -56,7 +48,6 @@ export default function LoginForm({ theme }: LoginFormProps) {
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [isEmailError, setIsEmailError] = useState<boolean>(false);
   const [isPasswordError, setIsPasswordError] = useState<boolean>(false);
-
   const togglePasswordVisibility = () => {
     setShowPassword(prevShowPassword => !prevShowPassword);
   };
@@ -92,27 +83,65 @@ export default function LoginForm({ theme }: LoginFormProps) {
 
   return (
     <Form theme={theme} onSubmit={event => submitForm(event)}>
-      <InformationLabel text="Email" gridColumn="1 / 6" gridRow="1 / 2" />
+      <InformationLabel
+        text="Email"
+        textSize="1.5rem"
+        textColor={theme.colors.primary}
+        gridColumn="1 / 6"
+        gridRow="1 / 2"
+      />
       <InformationInput
         type="email"
+        placeholder="abcd1234@email.com"
         maxLength={254}
         gridColumn="1 / 6"
         gridRow="2 / 3"
         isError={isEmailError}
         onChange={event => emailChange(event)}
       />
-      <InformationLabel text="Password" gridColumn="1 / 6" gridRow="3 / 4" />
+      <InformationLabel
+        text="Password"
+        textSize="1.5rem"
+        textColor={theme.colors.primary}
+        gridColumn="1 / 6"
+        gridRow="3 / 4"
+      />
       <InformationInput
         type={showPassword ? 'text' : 'password'}
+        placeholder=". . . . . . . . . ."
         maxLength={20}
         gridColumn="1 / 6"
         gridRow="4 / 5"
         isError={isPasswordError}
         onChange={event => passwordChange(event)}
       />
-      <ToggleButton type="button" onClick={togglePasswordVisibility}>
-        {showPassword ? 'Hide' : 'Show'}
-      </ToggleButton>
+      <TextButton
+        text={showPassword ? 'Hide' : 'Show'}
+        textSize=""
+        textColor={theme.colors.primary}
+        gridColumn="5 / 6"
+        gridRow="4 / 5"
+        justifyContent="center"
+        onClick={togglePasswordVisibility}
+      />
+      <TextButton
+        text="Forgot Password?"
+        textSize=""
+        textColor={theme.colors.secondary}
+        gridColumn="1 / 3"
+        gridRow="6 / 7"
+        justifyContent="flex-start"
+        onClick={togglePasswordVisibility}
+      />
+      <TextButton
+        text="Register"
+        textSize=""
+        textColor={theme.colors.secondary}
+        gridColumn="5 / 6"
+        gridRow="6 / 7"
+        justifyContent="flex-end"
+        onClick={togglePasswordVisibility}
+      />
       <SubmitButton text="Log In" gridColumn="2 / 5" gridRow="5 / 6" />
     </Form>
   );
