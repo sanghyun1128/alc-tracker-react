2ba2309861fa4b3f40c326f9106938cd18bb0f65
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-02 14:06:58 +0900

    refactor: update props for more common use

--- src/components/InformationLabel.tsx
+++ src/components/InformationLabel.tsx
@@ -8,20 +8,22 @@ const Label = styled.label<{
 }>`
   grid-column: ${props => props.$gridColumn};
   grid-row: ${props => props.$gridRow};
-  font-size: 1.5rem;
   font-weight: bold;
   font-family: inherit;
-  color: ${props => props.theme.colors.primary};
 `;
 
 interface InformationLabelProps {
   text: string;
+  textSize: string;
+  textColor: string;
   gridColumn: string;
   gridRow: string;
 }
 
 export default function InformationLabel({
   text,
+  textSize,
+  textColor,
   gridColumn,
   gridRow,
 }: InformationLabelProps) {
@@ -29,7 +31,8 @@ export default function InformationLabel({
     <Label
       htmlFor={text.toLowerCase()}
       $gridColumn={gridColumn}
-      $gridRow={gridRow}>
+      $gridRow={gridRow}
+      style={{ fontSize: textSize, color: textColor }}>
       {text}
     </Label>
   );
