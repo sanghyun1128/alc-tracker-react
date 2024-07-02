8978856e1841be57edf307301b9fe3bbad7c567e
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-02 14:24:54 +0900

    feat: add abnormal device size media query

--- src/const/deviceSizes.ts
+++ src/const/deviceSizes.ts
@@ -1,4 +1,5 @@
 export const deviceSizes = {
+  abnormal: '@media (max-width: 299px)',
   small: '@media (min-width: 300px)',
   medium: '@media (min-width: 768px)',
   large: '@media (min-width: 1200px)',
