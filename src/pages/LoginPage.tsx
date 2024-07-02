3db7fca27f17666c40d3e04568dd744c2a04188a
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-02 15:49:57 +0900

    refactor: remove not needed styled-components code

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
 
-import { styled } from 'styled-components';
-
 import LoginForm from '../forms/LoginForm';
 import { useTheme } from '../hooks/useTheme';
-
-const Container = styled.div`
-  display: grid;
-  place-items: center;
-  margin: 0;
-  padding: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${props => props.theme.colors.background};
-`;
 
 export default function LoginPage() {
   const [theme] = useTheme();
 
-  return (
-    <Container theme={theme}>
-      <LoginForm theme={theme} />
-    </Container>
-  );
+  return <LoginForm theme={theme} />;
 }
