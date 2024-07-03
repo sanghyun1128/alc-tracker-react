d034cb34a56eeea10a7db3834ba092d8a9f3d767
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-03 14:03:33 +0900

    design: add FormStyle component for consistent form styling

---
+++ src/styles/FormStyle.tsx
@@ -0,0 +1,30 @@
+import { styled } from 'styled-components';
+
+import { deviceSizes } from '../const/deviceSizes';
+
+const FormStyle = styled.form`
+  display: grid;
+  align-items: center;
+  justify-items: stretch;
+  border-radius: ${props => props.theme.formBorderRadius};
+  background-color: ${props => props.theme.colors.formBackground};
+
+  ${deviceSizes.abnormal} {
+    width: 250px;
+    padding: 20px 20px 0px 20px;
+  }
+  ${deviceSizes.small} {
+    width: 250px;
+    padding: 20px 20px 0px 20px;
+  }
+  ${deviceSizes.medium} {
+    width: 400px;
+    padding: 50px 50px 0px 50px;
+  }
+  ${deviceSizes.large} {
+    width: 500px;
+    padding: 50px 50px 0px 50px;
+  }
+`;
+
+export default FormStyle;
