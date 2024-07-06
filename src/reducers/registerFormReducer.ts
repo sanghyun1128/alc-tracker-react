b840311eb9d473d62e8f56440399a6e07a9b6834
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-06 14:26:36 +0900

    feat: add register form reducer

---
+++ src/reducers/registerFormReducer.ts
@@ -0,0 +1,33 @@
+import { RegisterFormState, RegisterFormAction } from '../types/registerForm';
+
+export const registerFormReducer = (
+  state: RegisterFormState,
+  action: RegisterFormAction,
+) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      return { ...state, name: action.payload };
+    case 'SET_EMAIL':
+      return { ...state, email: action.payload };
+    case 'SET_CONFIRM_EMAIL':
+      return { ...state, confirmEmail: action.payload };
+    case 'SET_BIRTH':
+      return { ...state, birth: action.payload };
+    case 'SET_GENDER':
+      return { ...state, gender: action.payload };
+    case 'SET_PASSWORD':
+      return { ...state, password: action.payload };
+    case 'SET_CONFIRM_PASSWORD':
+      return { ...state, confirmPassword: action.payload };
+    case 'SET_NAME_VALID':
+      return { ...state, isNameValid: action.payload };
+    case 'SET_EMAIL_VALID':
+      return { ...state, isEmailValid: action.payload };
+    case 'SET_EMAIL_CONFIRMED':
+      return { ...state, isEmailConfirmed: action.payload };
+    case 'SET_PASSWORD_VALID':
+      return { ...state, isPasswordValid: action.payload };
+    case 'SET_PASSWORD_CONFIRMED':
+      return { ...state, isPasswordConfirmed: action.payload };
+  }
+};
