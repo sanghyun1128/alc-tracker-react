b40a61592fc5adfd448fe1a09adaadfc5326e6fa
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-06 23:03:13 +0900

    refactor: change 'name' to 'nickname'

--- src/reducers/registerFormReducer.ts
+++ src/reducers/registerFormReducer.ts
@@ -6,7 +6,7 @@ export const registerFormReducer = (
 ) => {
   switch (action.type) {
     case 'SET_NICKNAME':
-      return { ...state, name: action.payload };
+      return { ...state, nickname: action.payload };
     case 'SET_EMAIL':
       return { ...state, email: action.payload };
     case 'SET_CONFIRM_EMAIL':
