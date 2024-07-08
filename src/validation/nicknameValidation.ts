0c0d07a1f39013c50a2a9506d7d02a3181f3079a
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-08 21:48:48 +0900

    fix: update nickname validation regex to allow whitespace

--- src/validation/nicknameValidation.ts
+++ src/validation/nicknameValidation.ts
@@ -2,12 +2,12 @@
  * Check nickname is following format
  *  - At least 2 characters
  *  - At most 15 characters
- *  - Only alphabets, numbers, and Korean characters
+ *  - Only allow alphanumeric characters, whitespace, and Korean characters
  *
  * @param {string} nickname
  * @returns {boolean}
  */
 export const nicknameValidation = (nickname: string): boolean => {
-  const regex = /^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z0-9가-힣]{2,15}$/;
+  const regex = /^[\w\sㄱ-ㅎㅏ-ㅣ가-힣]{2,15}$/;
   return regex.test(String(nickname));
 };
