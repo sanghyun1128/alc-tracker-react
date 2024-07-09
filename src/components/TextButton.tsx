97d853e43494a1997ea27e4ed36faaea45c31d26
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-09 21:00:32 +0900

    refactor: re add TextButton component for styled text buttons

---
+++ src/components/TextButton.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { styled } from 'styled-components';
+
+import TextButtonStyle from '../styles/TextButtonStyle';
+
+const Button = styled(TextButtonStyle)<{
+  $gridColumn: string;
+  $gridRow: string;
+  $justifyContent: string;
+}>`
+  grid-column: ${props => props.$gridColumn};
+  grid-row: ${props => props.$gridRow};
+  justify-content: ${props => props.$justifyContent};
+`;
+
+interface TextButtonProps {
+  text: string;
+  textSize: string;
+  textColor: string;
+  gridColumn: string;
+  gridRow: string;
+  justifyContent: string;
+  onClick: () => void;
+}
+
+export default function TextButton({
+  text,
+  textSize,
+  textColor,
+  gridColumn,
+  gridRow,
+  justifyContent,
+  onClick,
+}: TextButtonProps) {
+  return (
+    <Button
+      type="button"
+      style={{ fontSize: textSize, color: textColor }}
+      $gridColumn={gridColumn}
+      $gridRow={gridRow}
+      $justifyContent={justifyContent}
+      onClick={onClick}>
+      {text}
+    </Button>
+  );
+}
