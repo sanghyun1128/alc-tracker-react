8fed8697a5880ea9fb22ba846e5b5fbdf3c24bfc
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-11 18:47:24 +0900

    feat: make dropdown Selector component

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled from 'styled-components';
 
+import { Icons } from '../assets/svg';
 import { LabelStyle } from '../styles';
 
 const Container = styled.div<{ $gridColumn: string; $gridRow: string }>`
@@ -17,9 +18,50 @@ const Label = styled(LabelStyle)`
   grid-column: 1 / 2;
 `;
 
-const Select = styled.select`
+const SelectContainer = styled.div`
   grid-column: 2 / 7;
+  width: 100%;
+  position: relative;
+  margin: 0 auto;
 `;
+
+const SelectTrigger = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  background-color: #fff;
+  cursor: pointer;
+`;
+
+const SelectOptionList = styled.ul`
+  position: absolute;
+  width: 100%;
+  top: 100%;
+  left: 0;
+  border-top: none;
+  border-radius: 0 0 5px 5px;
+  background-color: #fff;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const SelectOption = styled.li`
+  padding: 10px;
+  cursor: pointer;
+
+  &:hover {
+    color: #fff;
+  }
+`;
+
+const ArrowIcon = styled(Icons.ARROW_LEFT).attrs({
+  width: 20,
+  height: 20,
+})``;
 
 interface SelectorProps {
   gridColumn: string;
@@ -40,6 +82,15 @@ export function Selector({
   options,
   onChange,
 }: SelectorProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
   return (
     <Container $gridColumn={gridColumn} $gridRow={gridRow}>
       <Label
@@ -47,13 +98,23 @@ export function Selector({
         style={{ fontSize: labelSize, color: labelColor }}>
         {labelText}
       </Label>
-      <Select id={labelText.toLowerCase()} onChange={onChange}>
-        {options.map((e, i) => (
-          <option key={e} value={i}>
-            {e}
-          </option>
-        ))}
-      </Select>
+      <SelectContainer>
+        <SelectTrigger onClick={toggleDropdown}>
+          {selectedOption}
+          <ArrowIcon />
+        </SelectTrigger>
+        {isOpen && (
+          <SelectOptionList>
+            {options.map((option, index) => (
+              <SelectOption
+                key={index}
+                onClick={() => handleOptionClick(option)}>
+                {option}
+              </SelectOption>
+            ))}
+          </SelectOptionList>
+        )}
+      </SelectContainer>
     </Container>
   );
 }
