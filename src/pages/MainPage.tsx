5f3da8c6142a81851ecfea42d145cbda2708a867
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-13 22:22:30 +0900

    feat: update MainPage to use IconButton component for displaying icons and handle icon click events

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+import IconButton from '../components/IconButton';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+`;
+
 export default function MainPage() {
-  return <div>MainPage</div>;
+  const navigate = useNavigate();
+
+  const handleIconClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    navigate(`/my/${e.currentTarget.id}`);
+  };
+
+  return (
+    <Container>
+      <IconButton icon="WINE" onClick={handleIconClick} size={100} />
+      <IconButton icon="WHISKEY" onClick={handleIconClick} size={100} />
+      <IconButton icon="COCKTAIL" onClick={handleIconClick} size={100} />
+    </Container>
+  );
 }
