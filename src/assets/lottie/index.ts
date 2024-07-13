512082c9e8e5e347e2a25945378f86d729fd430b
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-13 21:48:16 +0900

    remove: unused lottie animations

--- src/assets/lottie/index.ts
+++ src/assets/lottie/index.ts
@@ -1,21 +1,9 @@
 import { LottieComponentProps } from 'lottie-react';
 
-import CocktailAnimation from './cocktail.json';
 import NotFoundAnimation from './not-found.json';
-import WhiskeyAnimation from './whiskey.json';
-import WineAnimation from './wine.json';
 
 export const Animations: { [src: string]: LottieComponentProps } = {
-  COCKTAIL: {
-    animationData: CocktailAnimation,
-  },
   NOT_FOUND: {
     animationData: NotFoundAnimation,
-  },
-  WHISKEY: {
-    animationData: WhiskeyAnimation,
-  },
-  WINE: {
-    animationData: WineAnimation,
   },
 };
