94d914a8e639652249416ecfbd7f9cbeaa52df5f
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-13 21:48:45 +0900

    refactor: update LottieContainer to use flex for centering

--- src/components/LottieContainer.tsx
+++ src/components/LottieContainer.tsx
@@ -3,15 +3,25 @@ import React from 'react';
 import Lottie, { LottieComponentProps } from 'lottie-react';
 import { styled } from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
 
 interface LottieContainerProps {
   animation: LottieComponentProps;
+  style: React.CSSProperties;
 }
 
-export default function LottieContainer({ animation }: LottieContainerProps) {
+export default function LottieContainer({
+  animation,
+  style,
+}: LottieContainerProps) {
   return (
-    <Container>
+    <Container style={style}>
       <Lottie
         animationData={animation.animationData}
         style={{ width: '100%', height: '100%' }}
