625fbe2b2d47467a192e5047c95a776c42f222c7
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-13 21:50:25 +0900

    refactor: update NotFoundPage accordingly as LottieContainer changes

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -4,5 +4,10 @@ import { Animations } from '../assets/lottie';
 import LottieContainer from '../components/LottieContainer';
 
 export default function NotFoundPage() {
-  return <LottieContainer animation={Animations.NOT_FOUND} />;
+  return (
+    <LottieContainer
+      animation={Animations.NOT_FOUND}
+      style={{ width: '100%', height: '100%' }}
+    />
+  );
 }
