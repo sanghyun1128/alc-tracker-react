884b41e25283c652c74224c90a3c2b2585015376
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-13 22:17:51 +0900

    feat: add SVG icons for cocktails, whiskey, and wine

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,4 +1,7 @@
 import { ReactComponent as ArrowLeft } from './arrow-left.svg';
+import { ReactComponent as Cocktail } from './cocktail.svg';
+import { ReactComponent as Whiskey } from './whiskey.svg';
+import { ReactComponent as Wine } from './wine.svg';
 
 export type IconType = React.FunctionComponent<
   React.SVGProps<SVGSVGElement> & { title?: string | undefined }
@@ -6,4 +9,7 @@ export type IconType = React.FunctionComponent<
 
 export const Icons: { [src: string]: IconType } = {
   ARROW_LEFT: ArrowLeft,
+  WINE: Wine,
+  WHISKEY: Whiskey,
+  COCKTAIL: Cocktail,
 };
