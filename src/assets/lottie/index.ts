03ea4ed9eaf1d53e3d3b46ad2ef876fcc24061f4
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-13 20:54:48 +0900

    feat: add animations for cocktails, whiskey, and wine

--- src/assets/lottie/index.ts
+++ src/assets/lottie/index.ts
@@ -1,3 +1,21 @@
-import NotFoundAnimation from './not-found.json';
+import { LottieComponentProps } from 'lottie-react';
 
-export { NotFoundAnimation };
+import CocktailAnimation from './cocktail.json';
+import NotFoundAnimation from './not-found.json';
+import WhiskeyAnimation from './whiskey.json';
+import WineAnimation from './wine.json';
+
+export const Animations: { [src: string]: LottieComponentProps } = {
+  COCKTAIL: {
+    animationData: CocktailAnimation,
+  },
+  NOT_FOUND: {
+    animationData: NotFoundAnimation,
+  },
+  WHISKEY: {
+    animationData: WhiskeyAnimation,
+  },
+  WINE: {
+    animationData: WineAnimation,
+  },
+};
