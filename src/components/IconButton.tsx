ac1b10f953c824ca21b66db3f7abe541868f75d3
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-14 22:31:18 +0900

    feat: update IconButton component to include style and buttonColor attribute

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { shake } from '../animations/basicAnimations';
 import { Icons, IconType } from '../assets/svg';
 
-const Button = styled.button`
+const Button = styled.button<{ $buttonColor: string }>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -14,11 +14,17 @@ const Button = styled.button`
   cursor: pointer;
   padding: 10px;
   border-radius: 50%;
-  background-color: ${props => props.theme.colors.primary};
+  background-color: ${props =>
+    props.$buttonColor === 'primary'
+      ? props.theme.colors.primary
+      : props.theme.colors.secondary};
   transition: background-color 0.3s ease-in-out;
 
   &:hover {
-    background-color: ${props => props.theme.colors.primaryOn};
+    background-color: ${props =>
+      props.$buttonColor === 'primary'
+        ? props.theme.colors.primaryOn
+        : props.theme.colors.secondaryOn};
     animation: ${shake} 0.5s;
   }
 `;
@@ -26,6 +32,8 @@ const Button = styled.button`
 interface IconButtonProps {
   icon: keyof typeof Icons;
   size: number;
+  buttonColor: 'primary' | 'secondary';
+  style?: React.CSSProperties;
   onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
@@ -35,13 +43,21 @@ interface IconButtonProps {
  * @param {Number} props.size size of the icon
  * @param {Function} props.onClick function to be called when the button is clicked
  */
-export default function IconButton({ icon, size, onClick }: IconButtonProps) {
+export default function IconButton({
+  icon,
+  size,
+  buttonColor,
+  style = {},
+  onClick,
+}: IconButtonProps) {
   const Icon = Icons[icon] as IconType;
 
   return (
     <Button
       type="button"
       id={String(icon).toLowerCase()}
+      $buttonColor={buttonColor}
+      style={style}
       onClick={e => onClick(e)}>
       <Icon width={size} height={size} />
     </Button>
