253289e9c3a88cb99c0740dcd0023ed7d9db24bb
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-16 16:01:25 +0900

    feat: update TextLabel component to support different text sizes

--- src/components/TextLabel.tsx
+++ src/components/TextLabel.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-const Label = styled.h2`
+const H1 = styled.h1`
+  flex: 1 1 0;
+  font-weight: bold;
+  font-family: inherit;
+`;
+
+const H2 = styled.h2`
+  flex: 1 1 0;
+  font-weight: bold;
+  font-family: inherit;
+`;
+
+const H3 = styled.h3`
   flex: 1 1 0;
   font-weight: bold;
   font-family: inherit;
@@ -10,14 +22,18 @@ const Label = styled.h2`
 
 interface TextLabelProps {
   text: string;
+  size: 'h1' | 'h2' | 'h3';
   style: React.CSSProperties;
 }
 
 /**
  * @param {TextLabelProps} props
  * @param {String} props.text text to be displayed
+ * @param {String} props.size size of the text 'h1' | 'h2' | 'h3'
  * @param {Object} props.style React.CSSProperties to be applied to the text
  */
-export default function TextLabel({ text, style }: TextLabelProps) {
-  return <Label style={style}>{text}</Label>;
+export default function TextLabel({ text, size, style }: TextLabelProps) {
+  if (size === 'h1') return <H1 style={style}>{text}</H1>;
+  else if (size === 'h2') return <H2 style={style}>{text}</H2>;
+  else return <H3 style={style}>{text}</H3>;
 }
