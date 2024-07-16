3be8c4d97377bce0fc75d6aee34b317189c18314
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-16 16:23:11 +0900

    feat: add NO_DATA animation to Animations object

--- src/assets/lottie/index.ts
+++ src/assets/lottie/index.ts
@@ -1,9 +1,13 @@
 import { LottieComponentProps } from 'lottie-react';
 
+import NoData from './no-data.json';
 import NotFoundAnimation from './not-found.json';
 
 export const Animations: { [src: string]: LottieComponentProps } = {
   NOT_FOUND: {
     animationData: NotFoundAnimation,
+  },
+  NO_DATA: {
+    animationData: NoData,
   },
 };
