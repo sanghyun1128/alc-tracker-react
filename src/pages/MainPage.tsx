8ef3761a25bf45a9df71ee9b9d0d18029623316c
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-18 22:27:51 +0900

    feat: add CardView and EmptyView components to MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,8 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 import { fadeIn } from '../animations/basicAnimations';
+import CardView from '../components/CardView';
 import DotPagination from '../components/DotPagination';
+import EmptyView from '../components/EmptyView';
 import IconButton from '../components/IconButton';
+import IconLabel from '../components/IconLabel';
+import TextLabel from '../components/TextLabel';
+import { wineList } from '../const/dummy';
+import { useTheme } from '../hooks/useTheme';
 
 const Container = styled.div`
   display: flex;
@@ -27,6 +33,21 @@ const Body = styled.div`
   align-items: center;
 `;
 
+const MainViewSection = styled.div`
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-rows: repeat(10, 1fr);
+  align-items: center;
+  justify-items: center;
+  width: 100%;
+  height: 100%;
+  border: 0;
+  padding: 0;
+  margin: 0;
+  border-radius: ${props => props.theme.borderRadius};
+  background-color: ${props => props.theme.colors.secondary};
+`;
+
 const ControlSection = styled.div`
   display: flex;
   align-items: center;
@@ -35,6 +56,7 @@ const ControlSection = styled.div`
 `;
 
 export default function MainPage() {
+  const [theme] = useTheme();
   const navigate = useNavigate();
 
   const handleIconClick = (
@@ -51,6 +73,37 @@ export default function MainPage() {
           align="column"
           style={{ gridRow: '1 / 11', gridColumn: '1 / 2' }}
         />
+        <MainViewSection style={{ gridRow: '1 / 11', gridColumn: '2 / 11' }}>
+          <IconLabel
+            icon="WINE"
+            size={30}
+            style={{
+              gridRow: '1 / 2',
+              gridColumn: '1 / 2',
+              backgroundColor: `${theme.colors.primary}`,
+            }}
+          />
+          <TextLabel
+            text="Wine"
+            size="h2"
+            style={{
+              gridRow: '1 / 2',
+              gridColumn: '2 / 3',
+              color: `${theme.colors.textDark}`,
+            }}
+          />
+          {/* <EmptyView
+            theme={theme}
+            style={{
+              gridRow: '2 / 11',
+              gridColumn: '1 / 11',
+            }}
+          /> */}
+          <CardView
+            arr={wineList}
+            style={{ gridRow: '2 / 11', gridColumn: '1 / 11' }}
+          />
+        </MainViewSection>
       </Body>
 
       <ControlSection>
