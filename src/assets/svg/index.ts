da719e3b6a96a37501277b32037f8b9be15a632a
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-19 22:23:34 +0900

    feat: add new wine-related SVG icons and update IconType interface

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,8 +2,13 @@ import { ReactComponent as ArrowLeft } from './arrow-left.svg';
 import { ReactComponent as Cocktail } from './cocktail.svg';
 import { ReactComponent as Plus } from './plus.svg';
 import { ReactComponent as Setting } from './setting.svg';
+import { ReactComponent as StarEmpty } from './star-empty.svg';
+import { ReactComponent as StarFull } from './star-full.svg';
 import { ReactComponent as User } from './user.svg';
 import { ReactComponent as Whiskey } from './whiskey.svg';
+import { ReactComponent as WineRed } from './wine-red.svg';
+import { ReactComponent as WineSparkling } from './wine-sparkling.svg';
+import { ReactComponent as WineWhite } from './wine-white.svg';
 import { ReactComponent as Wine } from './wine.svg';
 
 export type IconType = React.FunctionComponent<
@@ -18,4 +23,9 @@ export const Icons: { [src: string]: IconType } = {
   SETTING: Setting,
   USER: User,
   PLUS: Plus,
+  STAR_EMPTY: StarEmpty,
+  STAR_FULL: StarFull,
+  WINE_RED: WineRed,
+  WINE_WHITE: WineWhite,
+  WINE_SPARKLING: WineSparkling,
 };
