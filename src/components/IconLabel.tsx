6afcc8617156fb3099f8c433f62b965bcc683692
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-19 22:42:38 +0900

    refactor: update IconLabel to handle missing icons gracefully

--- src/components/IconLabel.tsx
+++ src/components/IconLabel.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Icons, IconType } from '../assets/svg';
 
 interface IconLabelProps {
-  icon: keyof typeof Icons;
+  icon: string;
   size: number;
   style?: React.CSSProperties;
 }
@@ -11,11 +11,16 @@ interface IconLabelProps {
 /**
  * @param {IconLabelProps} props
  * @param {String} props.icon name of the icon to be displayed and used as the id of the button
+ * icon names are defined in src/assets/svg/index.ts
  * @param {Number} props.size size of the icon
  * @param {Object} props.style React.CSSProperties to be applied to the icon
  */
 export default function IconLabel({ icon, size, style = {} }: IconLabelProps) {
-  const Icon = Icons[icon] as IconType;
+  let Icon = Icons[icon] as IconType;
+
+  if (!Icon) {
+    Icon = Icons.FILE_EMPTY;
+  }
 
   return (
     <Icon
