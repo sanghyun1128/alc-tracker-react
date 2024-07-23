b54c1a4f8aeeaf5f7411a644c8faf4ad0ec412e3
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-23 18:14:14 +0900

    feat: add page and setPage props

--- src/components/DotPagination.tsx
+++ src/components/DotPagination.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { styled } from 'styled-components';
 
@@ -27,27 +27,36 @@ const Indicator = styled.button<{ $selected: boolean }>`
 interface IndicatorProps {
   numOfPages: number;
   align: 'row' | 'column';
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
   style: React.CSSProperties;
 }
 
+/**
+ * @param {IndicatorProps} props
+ * @param {Number} props.numOfPages number of pages to be displayed
+ * @param {String} props.align direction of the pagination
+ * @param {Number} props.page current page
+ * @param {Function} props.setPage function to set the current page
+ * @param {Object} props.style React.CSSProperties to be applied to the container
+ */
 export default function DotPagination({
   numOfPages,
   align = 'column',
+  page,
+  setPage,
   style,
 }: IndicatorProps) {
-  const [indicator, setIndicator] = useState<number>(0);
-
   const handleIndicatorClick = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
-    setIndicator(+e.currentTarget.id);
-    console.log(e.currentTarget);
+    setPage(+e.currentTarget.id);
   };
 
   return (
     <Container style={{ ...style, flexDirection: align }}>
       {[...Array(numOfPages)].map((_, index) => {
-        if (index === indicator) {
+        if (index === page) {
           return <Indicator key={index} id={String(index)} $selected={true} />;
         }
         return (
