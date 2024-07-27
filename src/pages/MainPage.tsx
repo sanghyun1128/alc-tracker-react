f78150f022ae9dcadb639f12637d43a00e242a79
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-27 22:06:39 +0900

    refactor: change to use default scrolling feature

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,13 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
-import { fadeIn, fadeOut } from '../animations/basicAnimations';
 import CardView from '../components/CardView';
 import DotPagination from '../components/DotPagination';
 import EmptyView from '../components/EmptyView';
 import IconButton from '../components/IconButton';
 import IconLabel from '../components/IconLabel';
-import TextLabel from '../components/TextLabel';
 import {
   cocktailCardList,
   whiskeyCardList,
@@ -20,21 +18,16 @@ import { useTheme } from '../hooks/useTheme';
 const Container = styled.div`
   display: grid;
   grid-template-columns: 0.07fr 1fr;
-  grid-template-rows: 1fr 0.1fr;
+  grid-template-rows: 0.1fr 1fr 0.1fr;
   padding: 10px;
   margin: 0;
   width: 80vw;
   height: 90vh;
 `;
 
-const MainViewSection = styled.div<{ $scrollProgress: number }>`
-  grid-row: 1 / 2;
+const MainViewSection = styled.div`
+  grid-row: 1 / 3;
   grid-column: 2 / 3;
-  display: grid;
-  grid-template-columns: repeat(10, 1fr);
-  grid-template-rows: repeat(10, 1fr);
-  align-items: center;
-  justify-items: center;
   border: 0;
   padding: 0;
   margin: 0;
@@ -42,12 +35,11 @@ const MainViewSection = styled.div<{ $scrollProgress: number }>`
   height: 100%;
   border-radius: ${props => props.theme.borderRadius};
   background-color: ${props => props.theme.colors.secondary};
-  animation: ${props => (props.$scrollProgress === 500 ? fadeOut : fadeIn)} 0.5s
-    ease-in-out;
+  overflow: scroll;
 `;
 
 const ControlSection = styled.div`
-  grid-row: 2 / 3;
+  grid-row: 3 / 4;
   grid-column: 2 / 3;
   display: flex;
   align-items: center;
@@ -69,39 +61,12 @@ export default function MainPage() {
     cocktailCardList,
   ]);
   const [pageIndex, setPageIndex] = useState<number>(0);
-  const [scrollProgress, setScrollProgress] = useState<number>(500);
 
   const handleIconClick = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     navigate(`/${e.currentTarget.id}`);
   };
-
-  const handleWheel = useCallback((e: React.WheelEvent) => {
-    // e.preventDefault();
-    const delta = e.deltaY > 0 ? 1 : -1;
-    setScrollProgress(prev => Math.min(Math.max(prev + delta * 10, 0), 1000));
-  }, []);
-
-  useEffect(() => {
-    if (scrollProgress >= 1000) {
-      setPageIndex(prevIndex =>
-        prevIndex < pageList.length - 1 ? prevIndex + 1 : 0,
-      );
-      setTimeout(() => {
-        setScrollProgress(500);
-      }, 1000);
-    } else if (scrollProgress <= 0) {
-      setScrollProgress(500);
-      setPageIndex(prevIndex =>
-        prevIndex > 0 ? prevIndex - 1 : pageList.length - 1,
-      );
-      setTimeout(() => {
-        setScrollProgress(500);
-      }, 1000);
-    }
-    console.log(scrollProgress);
-  }, [scrollProgress]);
 
   return (
     <Container>
@@ -110,39 +75,26 @@ export default function MainPage() {
         align="column"
         page={pageIndex}
         setPage={setPageIndex}
-        style={{ gridRow: '1 / 2', gridColumn: '1 / 2' }}
+        style={{ gridRow: '1 / 3', gridColumn: '1 / 2' }}
       />
-      <MainViewSection onWheel={handleWheel} $scrollProgress={scrollProgress}>
-        <IconLabel
-          icon={pageList[pageIndex][0]}
-          size={30}
-          style={{
-            gridRow: '1 / 2',
-            gridColumn: '1 / 2',
-            backgroundColor: `${theme.colors.primary}`,
-            borderRadius: '50%',
-            padding: '10px',
-          }}
-        />
-        <TextLabel
-          text={pageList[pageIndex][1]}
-          size="h2"
-          style={{
-            gridRow: '1 / 2',
-            gridColumn: '2 / 3',
-            color: `${theme.colors.textDark}`,
-          }}
-        />
-        {cardData[pageIndex].length === 0 ? (
-          <EmptyView
-            theme={theme}
-            style={{ gridRow: '2 / 11', gridColumn: '1 / 11' }}
-          />
-        ) : (
-          <CardView
-            arr={cardData[pageIndex]}
-            style={{ gridRow: '2 / 11', gridColumn: '1 / 11' }}
-          />
+      <IconLabel
+        icon={pageList[pageIndex][0]}
+        size={30}
+        style={{
+          gridRow: '1 / 2',
+          gridColumn: '1 / 2',
+          backgroundColor: `${theme.colors.primary}`,
+          borderRadius: '50%',
+          padding: '10px',
+        }}
+      />
+      <MainViewSection>
+        {pageList.map((page, index) =>
+          cardData[index].length === 0 ? (
+            <EmptyView key={index} theme={theme} style={{}} />
+          ) : (
+            <CardView key={index} arr={cardData[index]} style={{}} />
+          ),
         )}
       </MainViewSection>
 
