e272dc38e736f570c1fb60fe192624e164af2024
sanghyun1128 <sanghyun1128@gmail.com>
2024-07-29 21:51:28 +0900

    design: Update CardView and EmptyView components styling

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -13,6 +13,8 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
+  margin: 0;
+  padding: 0;
 `;
 
 interface CardViewProps {
--- src/components/EmptyView.tsx
+++ src/components/EmptyView.tsx
@@ -11,6 +11,10 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
 `;
 
 interface EmptyViewProps {
