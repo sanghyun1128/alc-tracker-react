4e64ad4d214fe22a6ae4efc9430e9560f84a20e8
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-02 22:50:16 +0900

    design: update SubmitButton component styles

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { ButtonStyle } from '../../styles';
-
-const Button = styled(ButtonStyle)<{ $gridColumn: string; $gridRow: string }>`
+const Button = styled.button<{ $gridColumn: string; $gridRow: string }>`
   grid-column: ${props => props.$gridColumn};
   grid-row: ${props => props.$gridRow};
+  width: auto;
+  min-height: 3rem;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: 600;
+  padding: calc(0.875rem - 1px) calc(1.5rem - 1px);
+  margin: 0;
+  border: 0;
+  border-radius: ${props => props.theme.borderRadius};
   background-color: ${props => props.theme.colors.primary};
   color: ${props => props.theme.colors.textDark};
+  position: relative;
+  text-decoration: none;
+  cursor: pointer;
+  touch-action: manipulation;
+  transition: all 250ms;
 
   &:hover {
     transform: translateY(-2px);
