e44e0b971e4482c87f0500031f31ffe98f55d8b4
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-05 20:57:12 +0900

    remove: unused style

--- src/styles/TextButtonStyle.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const TextButtonStyle = styled.button`
-  display: flex;
-  background: none;
-  border: none;
-  cursor: pointer;
-  color: ${props => props.theme.colors.primary};
-`;
