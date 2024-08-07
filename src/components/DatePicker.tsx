94dcea6b062386b269531975808b070e77e52ad0
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-07 22:11:32 +0900

    refactor: update DatePicker component styles

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,54 +1,78 @@
 import React from 'react';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import { InputStyle, LabelStyle } from '../styles';
+import { shake } from '../animations/basicAnimations';
 
-const Container = styled.div<{ $gridColumn: string; $gridRow: string }>`
+const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   grid-template-rows: 1fr;
   align-items: center;
-  grid-column: ${props => props.$gridColumn};
-  grid-row: ${props => props.$gridRow};
 `;
 
-const Label = styled(LabelStyle)`
+const Label = styled.label`
+  flex: 1 1 0;
+  font-weight: bold;
+  font-family: inherit;
   grid-column: 1 / 2;
 `;
 
-const Input = styled(InputStyle)`
+const Input = styled.input<{
+  $isError: boolean;
+}>`
   grid-column: 2 / 7;
   margin: 0;
+  flex: 2 1 0;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  border: 0px;
+  border-bottom: ${({ $isError }) =>
+    $isError
+      ? css`3px solid ${props => props.theme.colors.warning}`
+      : css`3px solid ${props => props.theme.colors.secondary}`};
+  background-color: transparent;
+  outline: none;
+  font-size: 1rem;
+  font-weight: bold;
+  font-family: inherit;
+  color: ${props => props.theme.colors.textLight};
+  animation: ${({ $isError }) =>
+    $isError &&
+    css`
+      ${shake} 1.3s
+    `};
+
+  &:focus {
+    transition: border-bottom 0.8s;
+    border-bottom: 3px solid ${props => props.theme.colors.secondaryOn};
+  }
 `;
 
 interface DatePickerProps {
-  gridColumn: string;
-  gridRow: string;
   isError: boolean;
   labelText: string;
-  labelSize: string;
-  labelColor: string;
+  style: React.CSSProperties;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 //TODO: Remake DatePicker with select dropdowns for day, month, and year
+/**
+ * @param {DatePickerProps} props
+ * @param {Boolean} props.isError if the input is in an error state
+ * @param {String} props.labelText the text to display as the label
+ * @param {Object} props.style React.CSSProperties to be applied to the container
+ * @param {Function} props.onChange function to be called when the input changes
+ */
 export default function DatePicker({
-  gridColumn,
-  gridRow,
   isError,
+  style,
   labelText,
-  labelSize,
-  labelColor,
   onChange,
 }: DatePickerProps) {
   return (
-    <Container $gridColumn={gridColumn} $gridRow={gridRow}>
-      <Label
-        htmlFor={labelText.toLowerCase()}
-        style={{ fontSize: labelSize, color: labelColor }}>
-        {labelText}
-      </Label>
+    <Container style={style}>
+      <Label htmlFor={labelText.toLowerCase()}>{labelText}</Label>
       <Input
         type="date"
         id={labelText.toLowerCase()}
