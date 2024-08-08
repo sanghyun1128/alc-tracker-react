37274e3288b71f3a42138baf96685a7faf7fafa8
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-08 20:44:03 +0900

    refactor: Move LoginForm and RegisterForm to components/forms directory

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,8 @@ import CardView from './CardView';
 import DatePicker from './DatePicker';
 import DotPagination from './DotPagination';
 import EmptyView from './EmptyView';
+import LoginForm from './forms/LoginForm';
+import RegisterForm from './forms/RegisterForm';
 import InformationInput from './InformationInput';
 import HeadingLabel from './labels/HeadingLabel';
 import IconLabel from './labels/IconLabel';
@@ -14,6 +16,7 @@ import Selector from './Selector';
 
 export { TextButton, IconButton, SubmitButton };
 export { IconLabel, HeadingLabel };
+export { LoginForm, RegisterForm };
 export {
   CardView,
   DatePicker,
