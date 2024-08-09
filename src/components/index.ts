8ea04b0a52dbaccb36f5d3a9cc7e48a79ab13f42
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-09 15:18:51 +0900

    refactor: update import path

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,29 +1,36 @@
 import IconButton from './buttons/IconButton';
 import SubmitButton from './buttons/SubmitButton';
 import TextButton from './buttons/TextButton';
-import CardView from './CardView';
-import DatePicker from './DatePicker';
-import DotPagination from './DotPagination';
+import CardView from './cards/CardView';
+import ReviewCard from './cards/ReviewCard';
 import EmptyView from './EmptyView';
+import DatePicker from './forms/DatePicker';
+import InformationInput from './forms/InformationInput';
 import LoginForm from './forms/LoginForm';
 import RegisterForm from './forms/RegisterForm';
-import InformationInput from './InformationInput';
+import Selector from './forms/Selector';
 import HeadingLabel from './labels/HeadingLabel';
 import IconLabel from './labels/IconLabel';
-import LottieContainer from './LottieContainer';
-import ReviewCard from './ReviewCard';
-import Selector from './Selector';
+import LottieContainer from './layout/LottieContainer';
+import DotPagination from './navigation/DotPagination';
 
+// Button Components
 export { TextButton, IconButton, SubmitButton };
+
+// Card Components
+export { CardView, ReviewCard };
+
+// Form Components
+export { DatePicker, InformationInput, Selector, LoginForm, RegisterForm };
+
+// Label Components
 export { IconLabel, HeadingLabel };
-export { LoginForm, RegisterForm };
-export {
-  CardView,
-  DatePicker,
-  DotPagination,
-  EmptyView,
-  InformationInput,
-  LottieContainer,
-  ReviewCard,
-  Selector,
-};
+
+// Layout Components
+export { LottieContainer };
+
+// Navigation Components
+export { DotPagination };
+
+// Other Components
+export { EmptyView };
