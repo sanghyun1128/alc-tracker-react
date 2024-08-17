d5cb39f8d1730ef82c610ef06fc9c220a0e06f24
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-17 22:52:33 +0900

    feat: set modal background dim and add modal close button

--- src/components/modals/ItemInputModal.tsx
+++ src/components/modals/ItemInputModal.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
 
-export default function ItemInputModal() {
-  return <div>ItemInputModal</div>;
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 100;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface ItemInputModalProps {
+  setModalOpen: (arg: boolean) => void;
+}
+
+export default function ItemInputModal({ setModalOpen }: ItemInputModalProps) {
+  return (
+    <Container>
+      <button onClick={() => setModalOpen(false)}>close</button>
+    </Container>
+  );
 }
