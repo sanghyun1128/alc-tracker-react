25d41323c0207d061d77a9eb8f7677851d16585c
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-19 18:09:46 +0900

    feat: update ItemInputForm component with styled-components and props

--- src/components/forms/ItemInputForm.tsx
+++ src/components/forms/ItemInputForm.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-export default function ItemInputForm() {
-  return <div>ItemInputForm</div>;
+import { styled } from 'styled-components';
+
+import { Alcohol } from '../../types/const';
+
+const Container = styled.div``;
+interface ItemInputFormProps {
+  inputType: Alcohol;
+  style: React.CSSProperties;
+}
+
+export default function ItemInputForm({
+  inputType,
+  style,
+}: ItemInputFormProps) {
+  return (
+    <Container style={style}>
+      <h1>{inputType}</h1>
+    </Container>
+  );
 }
