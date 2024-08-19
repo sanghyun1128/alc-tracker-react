933150ef493668f8fd2c06bd73ab0c5aea1af23e
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-19 18:09:35 +0900

    feat: update ItemInputModal component with new dependencies and styles

--- src/components/modals/ItemInputModal.tsx
+++ src/components/modals/ItemInputModal.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 import { styled } from 'styled-components';
 
-import { Alcohol } from '../../types/const';
-import IconButton from '../buttons/IconButton';
+import { IconButton, SubmitButton, ItemInputForm, Selector } from '..';
+import { Alcohol, AlcoholList } from '../../types/const';
 
 const Container = styled.div`
   width: 100vw;
@@ -13,10 +13,22 @@ const Container = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.6);
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const Modal = styled.div`
+  width: 80%;
+  height: 90%;
+  background-color: ${props => props.theme.colors.formBackground};
+  border-radius: ${props => props.theme.borderRadius};
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-rows: 1fr 8fr 1fr;
+  align-items: center;
+  justify-items: center;
 `;
 
 interface ItemInputModalProps {
@@ -26,15 +38,34 @@ interface ItemInputModalProps {
 export default function ItemInputModal({ setModalOpen }: ItemInputModalProps) {
   const [typeOfAlcohol, setTypeOfAlcohol] = useState<Alcohol>('WINE');
 
+  console.log(typeof AlcoholList);
   return (
     <Container>
-      <IconButton
-        icon="CLOSE"
-        size={20}
-        buttonColor="secondary"
-        style={{}}
-        onClick={() => setModalOpen(false)}
-      />
+      <Modal>
+        <Selector
+          labelText={''}
+          labelSize={''}
+          labelColor={''}
+          options={['WINE', 'WHISKEY', 'COCKTAIL']}
+          style={{ gridColumn: '1 / 5', gridRow: '1 / 2' }}
+          onChange={e => setTypeOfAlcohol(e.target.value as Alcohol)}
+        />
+        <IconButton
+          icon="CLOSE"
+          size={20}
+          buttonColor="primary"
+          style={{ gridColumn: '10 / 11', gridRow: '1 / 2' }}
+          onClick={() => setModalOpen(false)}
+        />
+        <ItemInputForm
+          inputType={typeOfAlcohol}
+          style={{ gridColumn: '1 / 11', gridRow: '2 / 3' }}
+        />
+        <SubmitButton
+          text="Add Item"
+          style={{ gridColumn: '5 / 7', gridRow: '3 / 4' }}
+        />
+      </Modal>
     </Container>
   );
 }
