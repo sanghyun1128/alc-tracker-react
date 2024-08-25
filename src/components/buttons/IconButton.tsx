0be8341b1d31cd67614e15e7c6fb2d8847289346
sanghyun1128 <sanghyun1128@gmail.com>
2024-08-25 23:15:34 +0900

    refactor: remove unnecessary animation from IconButton component

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { shake } from '../../animations/basicAnimations';
 import { Icons, IconType } from '../../assets/svg';
 
 const Button = styled.button<{ $buttonColor: string }>`
