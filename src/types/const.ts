3c1cbbc0201921e3f83fea2a8c14f357e2934e5f
sanghyun1128 <sanghyun1128@gmail.com>
2024-09-02 17:18:00 +0900

    feat: add subtypeList to const.ts

--- src/types/const.ts
+++ src/types/const.ts
@@ -1,2 +1,7 @@
 export const AlcoholList = ['WINE', 'WHISKEY', 'COCKTAIL'];
 export type Alcohol = 'WINE' | 'WHISKEY' | 'COCKTAIL';
+export const subtypeList = [
+  ['Red', 'White', 'Rose', 'Sparkling', 'Dessert', 'Etc'],
+  ['Whiskey', 'Brandy', 'Vodka', 'Gin', 'Rum', 'Tequila', 'Etc'],
+  ['Shacking', 'Stirring', 'Blending', 'Layering', 'Etc'],
+];
