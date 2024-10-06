b852be88b6d615437d3d5d59e6a5b5b8bbac1f49
sanghyun1128 <sanghyun1128@gmail.com>
2024-10-06 20:26:08 +0900

    feat: add slideUp and slideDown animations to basicAnimations.ts

--- src/animations/basicAnimations.ts
+++ src/animations/basicAnimations.ts
@@ -83,3 +83,21 @@ export const fadeOut = keyframes`
         animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
     }
 `;
+
+export const slideUp = keyframes`
+  from {
+    transform: translateY(100%);
+  }
+  to {
+    transform: translateY(0);
+  }
+`;
+
+export const slideDown = keyframes`
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(100%);
+  }
+`;
