815421a48a1cffe29ff6fba87e31946f0b63c957
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-20 15:55:43 +0900

    feat: create axios instance with request and response interceptors

---
+++ src/api/axios.ts
@@ -0,0 +1,41 @@
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+
+const instance: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:4000/',
+  // timeout: 15000,
+  // withCredentials: true,
+});
+
+// 요청 인터셉터 추가하기
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    // 요청이 전달되기 전에 작업 수행
+    // TODO: Validate request interface
+    return config;
+  },
+  (error: AxiosError) => {
+    // 요청 오류가 있는 작업 수행
+    return Promise.reject(error);
+  },
+);
+
+// 응답 인터셉터 추가하기
+instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 데이터가 있는 작업 수행
+    return response;
+  },
+  (error: AxiosError) => {
+    // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 오류가 있는 작업 수행
+    // TODO: Show alert modal or something else
+    return Promise.reject(error);
+  },
+);
+export default instance;
