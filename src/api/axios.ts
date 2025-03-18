765a00efa1b5fbd2f59c7fe6c184ae4af9da700f
sanghyun1128 <sanghyun1128@gmail.com>
2025-03-19 00:31:52 +0900

    fix: improve token refresh logic in axios interceptor

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -48,7 +48,6 @@ instance.interceptors.request.use(
       // 2. Set the accessToken to the Authorization header
       setAccessTokenToHeader(accessToken, instance.defaults.headers);
     } catch (error) {
-      // 1-1. If accessToken is not found, throw an error
       //TODO: Handle the error
       console.error('Error setting access token to header:', error);
     }
@@ -77,28 +76,35 @@ instance.interceptors.response.use(
 
   // 4xx, 5xx code response
   async (error: AxiosError) => {
-    const originalRequest = error.config as InternalAxiosRequestConfig;
+    // 1. Save the original request
+    const { config, response } = error;
+    const originalRequest = config as InternalAxiosRequestConfig;
 
     const {
       message: errorMessage,
       error: errorType,
       statusCode: errorStatusCode,
-    } = error.response?.data as {
+    } = response?.data as {
       message: string;
       error: string;
       statusCode: number;
     };
 
-    if (errorMessage === 'Invalid token') {
+    // 2. Check error is token expired
+    if (errorMessage === 'Invalid token' && !originalRequest._retry) {
+      // 2-1. Set the _retry flag to true
       originalRequest._retry = true;
       try {
+        // 2-2. Get new accessToken
         const response = await requests.getAccessToken();
         const newAccessToken = response.data.accessToken;
 
+        // 2-3. Set the new accessToken to the localStorage and header
         localStorage.setItem('accessToken', newAccessToken);
         setAccessTokenToHeader(newAccessToken, instance.defaults.headers);
         setAccessTokenToHeader(newAccessToken, originalRequest.headers);
 
+        // 2-4. Retry the original request with the new accessToken
         return instance(originalRequest);
       } catch (refreshError) {
         return Promise.reject(refreshError);
