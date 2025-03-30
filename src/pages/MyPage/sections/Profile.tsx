90a7c54d1dedb34cd141b67dc65a60aea4b0cc03
sanghyun1128 <sanghyun1128@gmail.com>
2025-03-30 19:58:50 +0900

    feat: get and show profileImage

--- src/pages/MyPage/sections/Profile.tsx
+++ src/pages/MyPage/sections/Profile.tsx
@@ -3,18 +3,69 @@ import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 
 import { requests } from '../../../api/request';
+import DefaultProfileSVG from '../../../assets/svg/cocktail.svg';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProfileImage = styled.img`
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const Nickname = styled.h2`
+  margin-top: 16px;
+  font-size: 24px;
+`;
+
+interface Profile {
+  id: string;
+  index: number;
+  nickname: string;
+  email: string;
+  password: string;
+  birth: string;
+  gender: string;
+  profileImage: ProfileImage | null;
+  profileComment: string | null;
+  profileLanguageISOAlpha2: string | null;
+  profileRegionISOAlpha2: string | null;
+  role: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface ProfileImage {
+  path: string;
+}
 
 export default function Profile() {
-  const [profile, setProfile] = useState(null);
+  const [profile, setProfile] = useState<Profile>();
+  const [profileImageSrc, setProfileImageSrc] =
+    useState<string>(DefaultProfileSVG);
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
         const response = await requests.getMyProfile();
-        console.log('My profile:', response);
+        console.log('My profile:', response.data);
         setProfile(response.data);
+        if (response.data.profileImage) {
+          const imageResponse = await requests.getImage(
+            response.data.profileImage.path,
+          );
+          console.log('Profile image:', imageResponse.data);
+          // Assume imageResponse.data is a URL to the fetched image
+          const blob = new Blob([imageResponse.data], { type: 'image/png' });
+          const imageUrl = URL.createObjectURL(blob);
+          setProfileImageSrc(imageUrl);
+        }
       } catch (error) {
         console.error('Failed to fetch profile:', error);
       }
@@ -23,5 +74,10 @@ export default function Profile() {
     fetchProfile();
   }, []);
 
-  return <Container>Profile</Container>;
+  return (
+    <Container>
+      <ProfileImage src={profileImageSrc} />
+      <Nickname>{profile?.nickname}</Nickname>
+    </Container>
+  );
 }
