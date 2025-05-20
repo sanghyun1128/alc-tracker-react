85049d3f6c32d4ed875e63e6846431abf4f00417
sanghyun1128 <sanghyun1128@gmail.com>
2025-05-20 20:25:54 +0900

    feat: wrap RegisterForm in a styled Container for improved layout

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import { styled } from 'styled-components';
+
 import { RegisterForm } from '../../components';
 import { useTheme } from '../../hooks/useTheme';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
+`;
 
 export default function RegisterPage() {
   const [theme] = useTheme();
 
-  return <RegisterForm theme={theme} />;
+  return (
+    <Container>
+      <RegisterForm theme={theme} />
+    </Container>
+  );
 }
