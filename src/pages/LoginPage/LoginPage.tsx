f95c5b36dfcacdb006ee7efd91e26234fd5d0af7
sanghyun1128 <sanghyun1128@gmail.com>
2025-05-20 20:20:04 +0900

    feat: wrap LoginForm in a styled Container for improved layout

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import { styled } from 'styled-components';
+
 import { LoginForm } from '../../components';
 import { useTheme } from '../../hooks/useTheme';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
 
 export default function LoginPage() {
   const [theme] = useTheme();
 
-  return <LoginForm theme={theme} />;
+  return (
+    <Container>
+      <LoginForm theme={theme} />
+    </Container>
+  );
 }
