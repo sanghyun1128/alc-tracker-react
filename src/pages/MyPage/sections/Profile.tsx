87f2d60cf31708d72da499f94dfaabaf723717a5
sanghyun1128 <sanghyun1128@gmail.com>
2025-05-20 16:20:03 +0900

    design: update grid layout and adjust component sizes in Profile section

--- src/pages/MyPage/sections/Profile.tsx
+++ src/pages/MyPage/sections/Profile.tsx
@@ -11,11 +11,16 @@ import { getProfileImage } from '../../../utils/profileImage';
 import CountryFlagIcon from '../components/CountryFlagIcon';
 
 const Container = styled.div`
+  grid-row: 1 / 2;
   display: grid;
-  grid-template-columns: 2fr 3fr 3fr 1fr;
-  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 2fr 2fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
   justify-items: stretch;
   align-items: center;
+
+  background-color: ${props => props.theme.colors.componentBackground};
+  margin: ${props => props.theme.margin};
+  border-radius: ${props => props.theme.borderRadius};
 `;
 
 const ProfileImage = styled.img`
@@ -27,7 +32,7 @@ const ProfileImage = styled.img`
 
 const ProfileImageWrapper = styled.div`
   grid-column: 1 / 2;
-  grid-row: 1 / 3;
+  grid-row: 1 / 4;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -35,7 +40,15 @@ const ProfileImageWrapper = styled.div`
 
 const NicknameWrapper = styled.div`
   grid-column: 2 / 3;
-  grid-row: 1 / 2;
+  grid-row: 1 / 3;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const BioWrapper = styled.div`
+  grid-column: 2 / 3;
+  grid-row: 2 / 4;
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -43,25 +56,17 @@ const NicknameWrapper = styled.div`
 
 const RegionWrapper = styled.div`
   grid-column: 3 / 4;
-  grid-row: 1 / 2;
+  grid-row: 2 / 3;
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-around;
   align-items: center;
 `;
 
 const ControlButtonWrapper = styled.div`
   grid-column: 4 / 5;
-  grid-row: 1 / 2;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-`;
-
-const BioWrapper = styled.div`
-  grid-column: 2 / 5;
   grid-row: 2 / 3;
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
 `;
 
@@ -119,8 +124,8 @@ export default function Profile() {
       <ControlButtonWrapper>
         <IconButton
           icon="EDIT"
-          size={30}
-          buttonColor="primary"
+          size={20}
+          buttonColor="secondary"
           onClick={e => console.log('Button clicked', e)}
         />
       </ControlButtonWrapper>
