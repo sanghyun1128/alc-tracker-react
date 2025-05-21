2bceae9ec172b164e9d65e13480b059afa7bece9
sanghyun1128 <sanghyun1128@gmail.com>
2025-05-21 17:14:15 +0900

    design: implement layout and pagination for Alcohols section

--- src/pages/MyPage/sections/Alcohols.tsx
+++ src/pages/MyPage/sections/Alcohols.tsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { styled } from 'styled-components';
 
-const Container = styled.div``;
+import { DotPagination, IconLabel } from '../../../components';
+
+const Container = styled.div`
+  grid-row: 2 / 3;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 10fr;
+  justify-items: stretch;
+  align-items: center;
+
+  background-color: ${props => props.theme.colors.componentBackground};
+  margin: ${props => props.theme.margin};
+  border-radius: ${props => props.theme.borderRadius};
+
+  overflow-y: auto;
+`;
+
+const CategoryWrapper = styled.div`
+  grid-column: 1 / 4;
+  grid-row: 1 / 2;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  margin: ${props => props.theme.margin};
+  border-radius: ${props => props.theme.borderRadius};
+  background-color: ${props => props.theme.colors.secondary};
+`;
 
 export default function Alcohols() {
-  return <Container>Alcohols</Container>;
+  const categoryList = [
+    ['WINE', 'Wine'],
+    ['WHISKEY', 'Whiskey'],
+    ['COCKTAIL', 'Cocktail'],
+  ];
+
+  const [categoryIndex, setCategoryIndex] = useState<number>(0);
+  const [pageIndex, setPageIndex] = useState<number>(0);
+
+  return (
+    <Container>
+      <CategoryWrapper>
+        {categoryList.map(([iconName], idx) => (
+          <IconLabel
+            key={idx}
+            icon={iconName}
+            size={30}
+            style={{
+              backgroundColor: 'red',
+              borderRadius: '5px',
+              padding: '10px',
+            }}
+          />
+        ))}
+      </CategoryWrapper>
+
+      <DotPagination
+        numOfPages={10}
+        align="row"
+        page={pageIndex}
+        setPage={setPageIndex}
+      />
+    </Container>
+  );
 }
