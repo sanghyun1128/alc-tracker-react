b692cadc90bd7bfe89661dd625ef1979d5bdee39
sanghyun1128 <sanghyun1128@gmail.com>
2025-05-27 17:33:58 +0900

    feat: add enums and order arrays for cocktail, spirit, and wine categories

---
+++ src/types/alcohols/CocktailCategoryEnum.ts
@@ -0,0 +1,27 @@
+export enum CocktailCategoryEnum {
+  CLASSIC = 'classic',
+  FRUIT = 'fruit',
+  SPARKLING = 'sparkling',
+  SMOOTHIE_SHAKE = 'smoothie_shake',
+  MOCKTAIL = 'mocktail',
+  ON_THE_ROCKS = 'on_the_rocks',
+  HERB_SPICE = 'herb_spice',
+  SUMMER = 'summer',
+  WINTER = 'winter',
+  DESSERT = 'dessert',
+  FUSION = 'fusion',
+}
+
+export const CocktailCategoryOrder: CocktailCategoryEnum[] = [
+  CocktailCategoryEnum.CLASSIC,
+  CocktailCategoryEnum.FRUIT,
+  CocktailCategoryEnum.SPARKLING,
+  CocktailCategoryEnum.SMOOTHIE_SHAKE,
+  CocktailCategoryEnum.MOCKTAIL,
+  CocktailCategoryEnum.ON_THE_ROCKS,
+  CocktailCategoryEnum.HERB_SPICE,
+  CocktailCategoryEnum.SUMMER,
+  CocktailCategoryEnum.WINTER,
+  CocktailCategoryEnum.DESSERT,
+  CocktailCategoryEnum.FUSION,
+];
---
+++ src/types/alcohols/WineCategoryEnum.ts
@@ -0,0 +1,19 @@
+export enum WineCategoryEnum {
+  RED = 'red',
+  WHITE = 'white',
+  ROSE = 'rose',
+  SPARKLING = 'sparkling',
+  DESSERT = 'dessert',
+  NATURAL = 'natural',
+  OTHER = 'other',
+}
+
+export const WineCategoryOrder: WineCategoryEnum[] = [
+  WineCategoryEnum.RED,
+  WineCategoryEnum.WHITE,
+  WineCategoryEnum.ROSE,
+  WineCategoryEnum.SPARKLING,
+  WineCategoryEnum.DESSERT,
+  WineCategoryEnum.NATURAL,
+  WineCategoryEnum.OTHER,
+];
