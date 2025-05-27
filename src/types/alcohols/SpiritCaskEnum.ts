fba934c64d8d46cabaf004a5eaca9ebe18259d10
sanghyun1128 <sanghyun1128@gmail.com>
2025-05-27 17:54:39 +0900

    feat: add SpiritCaskEnum and CaskOrder for categorizing spirit casks

---
+++ src/types/alcohols/SpiritCaskEnum.ts
@@ -0,0 +1,27 @@
+export enum SpiritCaskEnum {
+  OTHER = 'other',
+  BOURBON = 'bourbon',
+  COGNAC = 'cognac',
+  MADEIRA = 'madeira',
+  PORT = 'port',
+  RUM = 'rum',
+  SAUTERNES = 'sauternes',
+  SHERRY = 'sherry',
+  TEQUILA = 'tequila',
+  VIRGIN = 'virgin',
+  WINE = 'wine',
+}
+
+export const CaskOrder: SpiritCaskEnum[] = [
+  SpiritCaskEnum.OTHER,
+  SpiritCaskEnum.BOURBON,
+  SpiritCaskEnum.COGNAC,
+  SpiritCaskEnum.MADEIRA,
+  SpiritCaskEnum.PORT,
+  SpiritCaskEnum.RUM,
+  SpiritCaskEnum.SAUTERNES,
+  SpiritCaskEnum.SHERRY,
+  SpiritCaskEnum.TEQUILA,
+  SpiritCaskEnum.VIRGIN,
+  SpiritCaskEnum.WINE,
+];
