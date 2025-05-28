f6c9182794e8d7f091a604ae6272905b93f557d7
sanghyun1128 <sanghyun1128@gmail.com>
2025-05-28 17:21:42 +0900

    feat: add image assets and export Images object

---
+++ src/assets/image/index.ts
@@ -0,0 +1,11 @@
+import defaultCocktail from './default-cocktail.png';
+import defaultProfile from './default-profile.png';
+import defaultSpirit from './default-spirit.png';
+import defaultWine from './default-wine.png';
+
+export const Images = {
+  DEFAULT_COCKTAIL: defaultCocktail,
+  DEFAULT_PROFILE: defaultProfile,
+  DEFAULT_SPIRIT: defaultSpirit,
+  DEFAULT_WINE: defaultWine,
+};
