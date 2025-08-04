e9f66ba700e78f99d29ff4f401230a501a6b1300
sanghyun1128 <sanghyun1128@gmail.com>
2025-08-04 16:52:36 +0900

    design: add fade animation for button transitions

--- src/themes/GlobalStyle.tsx
+++ src/themes/GlobalStyle.tsx
@@ -1,6 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  .fade-btn-enter {
+    opacity: 0;
+    transform: scale(0.9);
+  }
+  .fade-btn-enter-active {
+    opacity: 1;
+    transform: scale(1);
+    transition: opacity 200ms, transform 200ms;
+  }
+  .fade-btn-exit {
+    opacity: 1;
+    transform: scale(1);
+  }
+  .fade-btn-exit-active {
+    opacity: 0;
+    transform: scale(0.9);
+    transition: opacity 200ms, transform 200ms;
+  }
   body {
     margin: 0;
     padding: 0;
