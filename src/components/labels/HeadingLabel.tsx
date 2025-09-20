bbce6108215b044cc8c00ce61c5c364d17750dff
sanghyun1128 <sanghyun1128@gmail.com>
2025-09-20 15:56:11 +0900

    refactor: remove unused style prop from HeadingLabel documentation

--- src/components/labels/HeadingLabel.tsx
+++ src/components/labels/HeadingLabel.tsx
@@ -50,7 +50,6 @@ interface HeadingLabelProps {
  * @param {String} props.text text to be displayed
  * @param {String} props.size size of the text 'h1' | 'h2' | 'h3'
  * @param {String} props.type color of the text 'light' | 'dark'
- * @param {Object} props.style React.CSSProperties to be applied to the text
  */
 export default function HeadingLabel({
   text,
