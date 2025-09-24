3da4158398969d1ca0ddcb31811412047edc25b2
sanghyun1128 <sanghyun1128@gmail.com>
2025-09-24 15:12:22 +0900

    refactor: enhance styling and remove size prop from CountryFlagIcon component

--- src/pages/MyPage/components/CountryFlagIcon.tsx
+++ src/pages/MyPage/components/CountryFlagIcon.tsx
@@ -5,23 +5,45 @@ import { styled } from 'styled-components';
 
 import { FlagIcons, FlagIconType } from '../../../assets/svg/flag';
 
-const Container = styled.div``;
+const Container = styled.div`
+  /* Layout */
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+
+  /* Box model */
+  height: 1em;
+  width: auto;
+
+  /* Visuals */
+  overflow: hidden;
+
+  /* Interaction / content sizing */
+  svg {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 interface CountryFlagIconProps {
   region: string;
-  size: number;
   description?: string;
 }
 
 /**
+ * CountryFlagIcon displays a country's flag based on the given region code.
+ * Icon sizes are responsive to the font size of the parent element.
+ * So you need to wrap this component in a container with the desired font size.
+ *
  * @param {CountryFlagProps} props
  * @param {String} props.region - name of the region to be displayed and used as the id of the button
  * region names are defined in src/assets/svg/flag/index.ts
- * @param {Number} props.size - size of the icon
+ * @param {String} props.description - description for the flag icon, shown as a tooltip on hover
  */
 export default function CountryFlagIcon({
   region,
-  size,
   description,
 }: CountryFlagIconProps) {
   let FlagIcon = FlagIcons[region.toUpperCase()] as FlagIconType;
@@ -33,8 +55,6 @@ export default function CountryFlagIcon({
   return (
     <Container>
       <FlagIcon
-        width={size}
-        height={size}
         data-tooltip-id="tooltip"
         data-tooltip-content={description || ''}
       />
